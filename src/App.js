844f64eba632afe92f3ef69d1ad4a3ee0c8454b8
Jia Jing <jiajing19932@gmail.com>
2019-04-21 16:37:10 +0800

    style: replicate dota 2 hero selection UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="sidebar">
+          <div className="sidebar-container">
+            <img src="https://firstblood.io/pages/wp-content/uploads/2018/07/dota-2-hero-guide-970x570.jpg" alt=""/>
+            <p className="name">Earth Shaker</p>
+            <p className="description">
+              Raigor Stonehoof, the Earthshaker, is a melee strength hero with several area of effect (AoE) disables, commonly played as a ganker or initiator.
+            </p>
+            <div className="actions">
+              <div className="like"></div>
+              <div className="seen"></div>
+              <div className="comment"></div>
+            </div>
+            <a href="#" className="view-more">Lock In</a>
+          </div>
+        </div>
+        <div className="card-list">        
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+        </div>
       </div>
     );
   }
