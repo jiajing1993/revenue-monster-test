e1af8d6ebf65c2edb410baf7bbecb881755601bc
Jia Jing <jiajing19932@gmail.com>
2019-04-23 20:59:25 +0800

    chore: use jsonp instead of axios

--- src/App.js
+++ src/App.js
@@ -1,56 +1,70 @@
 import React, { Component } from 'react';
+import jsonp from 'jsonp';
+
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      query: "anime",
+      cards: [],
+      selectedCard: null,
+    }
+  }
+
+  componentDidMount(){
+    jsonp(`http://www.behance.net/v2/projects?q=${this.state.query}&client_id=LDGQKFP7dsmkhIKUAGG67ChSDASj1cWD` ,null,
+      (err, data) => {
+        console.log(data)
+        this.setState({
+          cards: data.projects
+        })
+      })
+  }
+
+  handleClick = (index) => {
+    console.log(index)
+    this.setState({
+      selectedCard: this.state.cards[index]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <div className="sidebar">
-          <div className="sidebar-container">
-            <img src="https://firstblood.io/pages/wp-content/uploads/2018/07/dota-2-hero-guide-970x570.jpg" alt=""/>
-            <p className="name">Earth Shaker</p>
-            <p className="description">
-              Raigor Stonehoof, the Earthshaker, is a melee strength hero with several area of effect (AoE) disables, commonly played as a ganker or initiator.
-            </p>
-            <div className="actions">
-              <div className="like"></div>
-              <div className="seen"></div>
-              <div className="comment"></div>
-            </div>
-            <a href="#" className="view-more">Lock In</a>
-          </div>
+          {
+            this.state.selectedCard && (
+              <div className="sidebar-container">
+                <img src={this.state.selectedCard.covers['404']} alt=""/>
+                <p className="name">{ this.state.selectedCard.name }</p>
+                <p className="description">
+                  Raigor Stonehoof, the Earthshaker, is a melee strength hero with several area of effect (AoE) disables, commonly played as a ganker or initiator.
+                </p>
+                <div className="actions">
+                  <div className="like">{ this.state.selectedCard.stats.views }</div>
+                  <div className="seen">{ this.state.selectedCard.stats.appreciations }</div>
+                  <div className="comment">{ this.state.selectedCard.stats.comments }</div>
+                </div>
+                <a href={this.state.selectedCard.url} className="view-more" target="_blank">Lock In</a>
+              </div>
+            )
+          }
         </div>
-        <div className="card-list">        
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
+        <div className="card-list">
+          {
+            this.state.cards.map((card, index) => {
+              let className = 'card';
+              if (card === this.state.selectedCard) {
+                className += ' selected';
+              }
+              return (
+                <div className={className} key={card.id} onClick={() => this.handleClick(index)} style={{ backgroundImage: `url(${card.covers['404']})`}}>
+                </div>
+              )
+            })
+          }      
         </div>
       </div>
     );
